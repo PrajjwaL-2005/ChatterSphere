4bc9674d05f67c3b95434b45edf93d014a35de87
realsaksham <sakshamtiwarimail@gmail.com>
2025-03-30 21:29:30 +0530

    added Post Component

--- backend/controllers/postControllers.js
+++ backend/controllers/postControllers.js
@@ -1,182 +1,291 @@
-import { Post } from "../models/postModel.js";
-import TryCatch from "../utlis/Trycatch.js";
-import getDataUrl from "../utlis/urlGenerator.js";
-import cloudinary from 'cloudinary';
+import React, { useEffect, useState } from "react";
+import { BsChatFill, BsThreeDotsVertical } from "react-icons/bs";
+import { IoHeartOutline, IoHeartSharp } from "react-icons/io5";
+import { UserData } from "../context/UserContext";
+import { PostData } from "../context/PostContext";
+import { format } from "date-fns";
+import { Link } from "react-router-dom";
+import { MdDelete } from "react-icons/md";
+import SimpleModal from "./SimpleModal";
+import { LoadingAnimation } from "./Loading";
+import toast from "react-hot-toast";
+import axios from "axios";
+import LikeModal from "./LikeModal";
+import { SocketData } from "../context/SocketContext";
 
+const PostCard = ({ type, value }) => {
+  const [isLike, setIsLike] = useState(false);
+  const [show, setShow] = useState(false);
+  const { user } = UserData();
+  const { likePost, addComment, deletePost, loading, fetchPosts } = PostData();
 
-export const newPost =  TryCatch(async(req,res)=>{
-    const {caption} = req.body
-    const ownerId = req.user._id ;
-    const file = req.file 
-    const fileUrl = getDataUrl(file)
+  const formatDate = format(new Date(value.createdAt), "MMMM do");
 
-    let option 
-    const type = req.query.type
-    if(type === "reel"){
-        option = {
-            resource_type: "video"
-
-        }
+  useEffect(() => {
+    for (let i = 0; i < value.likes.length; i++) {
+      if (value.likes[i] === user._id) setIsLike(true);
     }
-    else{
-        option = {} ;
+  }, [value, user._id]);
 
+  const likeHandler = () => {
+    setIsLike(!isLike);
+
+    likePost(value._id);
+  };
+
+  const [comment, setComment] = useState("");
+
+  const addCommentHandler = (e) => {
+    e.preventDefault();
+    addComment(value._id, comment, setComment, setShow);
+  };
+
+  const [showModal, setShowModal] = useState(false);
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  const deleteHandler = () => {
+    deletePost(value._id);
+  };
+
+  const [showInput, setShowInput] = useState(false);
+  const editHandler = () => {
+    setShowModal(false);
+    setShowInput(true);
+  };
+
+  const [caption, setCaption] = useState(value.caption ? value.caption : "");
+  const [captionLoading, setCaptionLoading] = useState(false);
+
+  async function updateCaption() {
+    setCaptionLoading(true);
+    try {
+      const { data } = await axios.put("/api/post/" + value._id, { caption });
+
+      toast.success(data.message);
+      fetchPosts();
+      setShowInput(false);
+      setCaptionLoading(false);
+    } catch (error) {
+      toast.error(error.response.data.message);
+      setCaptionLoading(false);
     }
+  }
 
-    const myCloud =  await cloudinary.v2.uploader.upload(fileUrl.content , option) ;
+  const [open, setOpen] = useState(false);
 
-    const post = await Post.create({
-        caption , 
-        post: {
-            id : myCloud.public_id ,
-            url : myCloud.secure_url, 
-        },
-        owner : ownerId , 
-        type : type ,
+  const oncloseLIke = () => {
+    setOpen(false);
+  };
 
+  const { onlineUsers } = SocketData();
 
-    });
+  return (
+    <div className="bg-gray-100 flex items-center justify-center pt-3 pb-14">
+      <SimpleModal isOpen={showModal} onClose={closeModal}>
+        <LikeModal isOpen={open} onClose={oncloseLIke} id={value._id} />
+        <div className="flex flex-col items-center justify-center gap-3">
+          <button
+            onClick={editHandler}
+            className="bg-blue-400 text-white py-1 px-3 rounded-md"
+          >
+            Edit
+          </button>
+          <button
+            onClick={deleteHandler}
+            className="bg-red-400 text-white py-1 px-3 rounded-md"
+            disabled={loading}
+          >
+            {loading ? <LoadingAnimation /> : "Delete"}
+          </button>
+        </div>
+      </SimpleModal>
+      <div className="bg-white p-8 rounded-lg shadow-md max-w-md">
+        <div className="flex items-center space-x-2">
+          <Link
+            className="flex items-center space-x-2"
+            to={`/user/${value.owner._id}`}
+          >
+            <img
+              src={value.owner.profilePic.url}
+              alt=""
+              className="w-8 h-8 rounded-full"
+            />
 
-   res.status(201).json({
-    message : "Post created" ,
-    post , 
+            {onlineUsers.includes(value.owner._id) && (
+              <div className="text-5xl font-bold text-green-400">.</div>
+            )}
 
-   }) ;
-});
+            <div>
+              <p className="text-gray-800 font-semibold">{value.owner.name}</p>
+              <div className="text-gray-500 text-sm">{formatDate}</div>
+            </div>
+          </Link>
 
-export const  deletePost = TryCatch(async(req , res )=>{
-    const post = await Post.findById(req.params.id) ;
-    if(!post)
-        return res.status(404).json({
-    message : "No post  with this id " , }) ;
+          {value.owner._id === user._id && (
+            <div className="text-gray-500 cursor-pointer">
+              <button
+                onClick={() => setShowModal(true)}
+                className="hover:bg-gray-50 rounded-full p-1 text-2xl"
+              >
+                <BsThreeDotsVertical />
+              </button>
+            </div>
+          )}
+        </div>
 
-    if(post.owner.toString() !== req.user._id.toString())
-        return res.status(403).json({
-            message: "Unauthorized" ,
-        }) ;
-    
-    await cloudinary.v2.uploader.destroy(post.post.id) ;
-    await post.deleteOne() ;
-    res.json({
-        message: "Post Deleted" , 
-    }) ;
+        <div className="mb-4">
+          {showInput ? (
+            <>
+              <input
+                className="custom-input"
+                style={{ width: "150px" }}
+                type="text"
+                placeholder="Enter Caption"
+                value={caption}
+                onChange={(e) => setCaption(e.target.value)}
+                required
+              />
+              <button
+                onClick={updateCaption}
+                className="text-sm bg-blue-500 text-white px-1 py-1 rounded-md"
+                disabled={captionLoading}
+              >
+                {captionLoading ? <LoadingAnimation /> : "Update Caption"}
+              </button>
+              <button
+                className="text-sm bg-red-500 text-white px-1 py-1 rounded-md"
+                onClick={() => setShowInput(false)}
+              >
+                X
+              </button>
+            </>
+          ) : (
+            <p className="text-gray-800">{value.caption}</p>
+          )}
+        </div>
 
-}) ;
+        <div className="mb-4">
+          {type === "post" ? (
+            <img
+              src={value.post.url}
+              alt=""
+              className="object-cover rounded-md"
+            />
+          ) : (
+            <video
+              src={value.post.url}
+              alt=""
+              className="w-[450px] h-[600px] object-cover rounded-md"
+              autoPlay
+              controls
+            />
+          )}
+        </div>
+        <div className="flex items-center justify-between text-gray-500">
+          <div className="flex items-center space-x-2">
+            <span
+              onClick={likeHandler}
+              className="text-red-500 text-2xl cursor-pointer"
+            >
+              {isLike ? <IoHeartSharp /> : <IoHeartOutline />}
+            </span>
+            <button
+              className="hover:bg-gray-50 rounded-full p-1"
+              onClick={() => setOpen(true)}
+            >
+              {value.likes.length} likes
+            </button>
+          </div>
+          <button
+            className="flex justify-center items-center gap-2 px-2 hover:bg-gray-50 rounded-full p-1"
+            onClick={() => setShow(!show)}
+          >
+            <BsChatFill />
+            <span>{value.comments.length} comments</span>
+          </button>
+        </div>
+        {show && (
+          <form onSubmit={addCommentHandler} className="flex gap-3">
+            <input
+              type="text"
+              className="custom-input"
+              placeholder="Enter Comment"
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+            />
+            <button className="bg-gray-100 rounded-lg px-5 py-2" type="submit">
+              Add
+            </button>
+          </form>
+        )}
 
+        <hr className="mt-2 mb-2" />
+        <p className="text-gray-800 font-semibold">Comments</p>
+        <hr className="mt-2 mb-2" />
+        <div className="mt-4">
+          <div className="comments max-h-[200px] overflow-y-auto">
+            {value.comments && value.comments.length > 0 ? (
+              value.comments.map((e) => (
+                <Comment
+                  value={e}
+                  key={e._id}
+                  user={user}
+                  owner={value.owner._id}
+                  id={value._id}
+                />
+              ))
+            ) : (
+              <p>No Comments</p>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export const getAllPosts = TryCatch(async(req,res)=>{
-    const posts = await Post.find({type:"post"})
-    .sort({createdAt:-1}).populate("owner" , "-password") ;
+export default PostCard;
 
-    const reels = await Post.find({type:"reel"})
-    .sort({createdAt:-1}).populate("owner" , "-password") ;
+export const Comment = ({ value, user, owner, id }) => {
+  const { deleteComment } = PostData();
 
-    res.json({posts , reels}) ;
-}) ;
+  const deleteCommentHandler = () => {
+    deleteComment(id, value._id);
+  };
+  return (
+    <div className="flex items-center space-x-2 mt-2">
+      <Link to={`/user/${value.user._id}`}>
+        <img
+          src={value.user.profilePic.url}
+          className="w-8 h-8 rounded-full"
+          alt=""
+        />
+      </Link>
+      <div>
+        <p className="text-gray-800 font-semibold">{value.user.name}</p>
+        <p className="text-gray-500 text-sm">{value.comment}</p>
+      </div>
 
-export const  likeUnlikePost = TryCatch(async(req,res)=>{
-    const post = await Post.findById(req.params.id) ; 
-    if(!post)
-        return res.status(404).json({
-            message : "No Post with this id" ,
-        }) ;
-    if(post.likes.includes(req.user._id)){
-        const index = post.likes.indexOf(req.user._id) ;
-        post.likes.splice(index,1 ) ;
-        await post.save() ;
-        res.json({
-            message : "Post Unlike" ,
-        }) ;
-    }
-    else{
-        post.likes.push(req.user._id) ;
+      {owner === user._id ? (
+        ""
+      ) : (
+        <>
+          {value.user._id === user._id && (
+            <button onClick={deleteCommentHandler} className="text-red-500">
+              <MdDelete />
+            </button>
+          )}
+        </>
+      )}
 
-        await post.save() ;
-        res.json({
-            message : "Post liked" ,
-        }) ;
-    }
-}) ;
-
-// Exporting the commentonPost function wrapped with TryCatch for error handling
-export const commentonPost = TryCatch(async(req,res)=>{
-    const post = await Post.findById(req.params.id) ;
-    if(!post)
-        return res.status(404).json({
-            message : "No Post with this id" ,
-        }) ;
-
-    post.comments.push({
-        user  : req.user._id ,
-        name: req.user.name , 
-        comment: req.body.comment 
-    }) ;
-    res.json({
-        message : "Comment Added" ,
-    }) ;
-}) ;
-
-
-
-
-export const deleteComment = TryCatch(async(req,res)=>{
-    const post = await Post.findById(req.params.id) ;
-    if(!post)
-        return res.status(404).json({
-            message : "No Post with this id" ,
-        }) ;
-    
-    if(!req.body.commentId) return res.status(404).json({
-        message : "Please give comment id" ,
-    });
-    
-    const commentIndex = post.comments.findIndex(
-        (item) => item._id.toString() === req.body.commentId.toString()
-    ) ;
-
-    if(commentIndex === -1){
-        return res.status(400).json({
-            message : " Comment not found" ,
-        }) ;
-
-    }
-
-    const comment = post.comments[commentIndex] ; 
-    if(post.owner.toString() === req.user._id.toString() || comment.user.toString() === req.user._id.toString()){
-        post.comments.splice(commentIndex , 1) ;
-         await post.save() ;
-         return res.json({
-            message : "Comment Deleted" ,
-         }) ;
-    } else {
-        return res.status(400).json({
-            message:"You are not allowed to delete this comment" ,
-        }) ;
-    }
-}) ;
-    
-
-
-
-
-
-
-export const editCaption =  TryCatch(async(req, res)=>{
-    const post = await Post.findById(req.params.id) ;
-    if(!post)
-        return res.status(404).json({
-            message : "No Post with this id" ,
-        }) ;
-
-    if(post.owner.toString() !== req.user._id.toString()) return res.status(403).json({
-        message : "you are not owner of this post" ,
-    }) ;
-
-    post.caption = req.body.caption ;
-    await post.save() ;
-
-    res.json({
-        message: "post updated" ,
-    }) ;
-
-}) ;
+      {owner === user._id && (
+        <button onClick={deleteCommentHandler} className="text-red-500">
+          <MdDelete />
+        </button>
+      )}
+    </div>
+  );
+};
