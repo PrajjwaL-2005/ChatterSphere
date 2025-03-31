5c61b336ba81cd073337a45d7e3cc385c80e0003
realsaksham <sakshamtiwarimail@gmail.com>
2025-04-01 00:07:53 +0530

    added loading component

--- backend/routes/postROutes.js
+++ backend/routes/postROutes.js
@@ -1,21 +1,28 @@
-import express from "express" 
+import express from "express";
 import { isAuth } from "../middlewares/isAuth.js";
-import { commentonPost, 
-    deleteComment, deletePost , 
-    editCaption, getAllPosts, 
-    likeUnlikePost, newPost } from "../controllers/postControllers.js";
+import {
+  commentonPost,
+  deleteComment,
+  deletePost,
+  editCaption,
+  getAllPosts,
+  likeUnlikePost,
+  newPost,
+} from "../controllers/postControllers.js";
 import uploadFile from "../middlewares/multer.js";
 
+const router = express.Router();
 
-const router = express.Router() ;
-router.post("/new" , isAuth ,uploadFile , newPost) ;
+router.post("/new", isAuth, uploadFile, newPost);
 
-router.delete("/:id" , isAuth , deletePost) ;
-router.put("/:id" , isAuth , editCaption) ;
+router.put("/:id", isAuth, editCaption);
+router.delete("/:id", isAuth, deletePost);
 
-router.get("/all" , isAuth , getAllPosts) ;
-router.post("/like/:id" , isAuth , likeUnlikePost) ;
-router.post("/comment/:id" , isAuth , commentonPost) ;
-router.delete("/comment/:id" , isAuth , deleteComment) ;
+router.get("/all", isAuth, getAllPosts);
+
+router.post("/like/:id", isAuth, likeUnlikePost);
+
+router.post("/comment/:id", isAuth, commentonPost);
+router.delete("/comment/:id", isAuth, deleteComment);
 
 export default router;
