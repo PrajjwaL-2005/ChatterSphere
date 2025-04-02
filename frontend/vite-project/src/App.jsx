d81f016a05b7af4ebbbffb5a42c84f2bfb35eb63
AKY216378 <aky47772@gmail.com>
2025-04-02 19:05:24 +0530

    chat page added

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { UserData } from "./context/UserContext";
+import Account from "./pages/Account";
+import NavigationBar from "./components/NavigationBar";
+import NotFound from "./components/NotFound";
+import Reels from "./pages/Reels";
+import { Loading } from "./components/Loading";
+import UserAccount from "./pages/UserAccount";
+import Search from "./pages/Search";
+import ChatPage from "./pages/ChatPage";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const { loading, isAuth, user } = UserData();
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {loading ? (
+        <Loading />
+      ) : (
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={isAuth ? <Home /> : <Login />} />
+            <Route path="/reels" element={isAuth ? <Reels /> : <Login />} />
+            <Route
+              path="/account"
+              element={isAuth ? <Account user={user} /> : <Login />}
+            />
+            <Route
+              path="/user/:id"
+              element={isAuth ? <UserAccount user={user} /> : <Login />}
+            />
+            <Route path="/login" element={!isAuth ? <Login /> : <Home />} />
+            <Route
+              path="/register"
+              element={!isAuth ? <Register /> : <Home />}
+            />
+            <Route path="*" element={<NotFound />} />
+            <Route
+              path="/register"
+              element={!isAuth ? <Register /> : <Home />}
+            />
+            <Route path="/search" element={isAuth ? <Search /> : <Login />} />
+            <Route
+              path="/chat"
+              element={isAuth ? <ChatPage user={user} /> : <Login />}
+            />
+          </Routes>
+          {isAuth && <NavigationBar />}
+        </BrowserRouter>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- frontend/vite-project/src/main.jsx
+++ frontend/vite-project/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { UserContextProvider } from "./context/UserContext.jsx";
+import { PostContextProvider } from "./context/PostContext.jsx";
+import { ChatContextProvider } from "./context/ChatContext.jsx";
+import { SocketContextProvider } from "./context/SocketContext.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <UserContextProvider>
+      <PostContextProvider>
+        <ChatContextProvider>
+          <SocketContextProvider>
+            <App />
+          </SocketContextProvider>
+        </ChatContextProvider>
+      </PostContextProvider>
+    </UserContextProvider>
+  </React.StrictMode>
+);
